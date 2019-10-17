230739c09abadf079d618ab2abf27a0d57f11561
IlyaSenko <illyax2000@gmail.com>
2019-10-17 11:11:32 +0300

    showup commit

--- server/index.js
+++ server/index.js
@@ -7,17 +7,34 @@ const app = express();
 
 app.use('/css', express.static(path.resolve(__dirname, '../src/css')));
 app.use('/img', express.static(path.resolve(__dirname, '../src/img')));
+app.use('/images', express.static(path.resolve(__dirname, '../src/images')));
 
-app.set('view engine', 'pug')
-app.set('views', path.resolve(__dirname, '../src/pug'))
+// app.set('view engine', 'pug')
+// app.set('views', path.resolve(__dirname, '../src/pug'))
+
+// app.get('/', function (req, res) {
+//   res.render('about')
+// })
 
 app.get('/', function (req, res) {
-  res.render('about')
+  res.sendFile(path.resolve(__dirname, '../src/pug/test.html'))
+})
+
+app.get('/news', function (req, res) {
+  res.sendFile(path.resolve(__dirname, '../src/pug/news.html'))
+})
+
+app.get('/business', function (req, res) {
+  res.sendFile(path.resolve(__dirname, '../src/pug/business.html'))
+})
+
+app.get('/business', function (req, res) {
+  res.sendFile(path.resolve(__dirname, '../src/pug/purchase.html'))
 })
 
 pages.forEach(item => {
   app.get(`/${item}`, function (req, res) {
-    res.render(item)
+    res.sendFile(path.resolve(__dirname, `../src/pug/${item}.html`))
   })
 })
 
